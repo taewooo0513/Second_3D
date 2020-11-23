07991427f7ba419a08bd6bab6d114c64354accf9
creatorSeraph <dbgksdl00@gmail.com>
2020-11-24 06:31:01 +0900

    * ImageManager의 lock을 shared_lock으로 변경(읽기/쓰기 구분)

--- cImageManager.cpp
+++ cImageManager.cpp
@@ -1,6 +1,7 @@
 #include "DXUT.h"
 #include "cImageManager.h"
 #include <mutex>
+#include <shared_mutex>
 
 cImageManager::cImageManager()
 {
@@ -21,18 +22,20 @@ cImageManager::~cImageManager()
 	mMesh.clear();
 }
 
-mutex imageMutex;
+shared_mutex imageMutex;
 Texture * cImageManager::AddImage(const string key, const string path)
 {
+	shared_lock readLock(imageMutex);
 	auto find = image.find(key);
 	if (find == image.end())
 	{
+		readLock.unlock();
 		LPDIRECT3DTEXTURE9 texturePtr;
 		D3DXIMAGE_INFO info;
 		if (D3DXCreateTextureFromFileExA(g_Device, path.c_str(), -2, -2, 0, 0, D3DFMT_UNKNOWN, D3DPOOL_DEFAULT, -1, -1, 0, &info, nullptr, &texturePtr) == S_OK)
 		{
-			lock_guard<mutex> lock(imageMutex);
 			Texture * text = new Texture(texturePtr, info);
+			lock_guard writeLock(imageMutex);
 			image[key] = text;
 			return text;
 		}
@@ -43,26 +46,30 @@ Texture * cImageManager::AddImage(const string key, const string path)
 
 Texture * cImageManager::FindImage(const string key)
 {
+	shared_lock readLock(imageMutex);
 	auto find = image.find(key);
-	if (find== image.end())
+	if (find == image.end())
 	{
 		return nullptr;
 	}
 	return find->second;
 }
 
-mutex meshMutex;
+shared_mutex meshMutex;
 Mesh * cImageManager::AddMesh(const string key, const string path)
 {
+	shared_lock readLock(meshMutex);
 	auto find = mMesh.find(key);
 	if (find != mMesh.end())
 	{
 		return find->second;
 	}
+	readLock.unlock();
+
 	Mesh*mesh = new Mesh;
 	cMeshLoader loader;
 	loader.ObjLoad(mesh, path);
-	lock_guard<mutex> lock(meshMutex);
+	lock_guard lock(meshMutex);
 	mMesh[key] = mesh;
  	return mMesh[key];
 
@@ -70,6 +77,7 @@ Mesh * cImageManager::AddMesh(const string key, const string path)
 
 Mesh * cImageManager::FindMesh(const string key)
 {
+	shared_lock readLock(meshMutex);
 	auto find = mMesh.find(key);
 	if (find == mMesh.end())
 	{
