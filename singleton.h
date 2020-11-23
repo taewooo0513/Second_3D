1d9f75764337dfba01f0702abaadfbe8049ce637
creatorSeraph <dbgksdl00@gmail.com>
2020-11-24 06:31:42 +0900

    * singleton을 thread_safe하게 변경(약간 맘에 안들긴 하지만 당장은 이렇게 처리하는게 맞을 듯)

--- singleton.h
+++ singleton.h
@@ -1,26 +1,27 @@
 #pragma once
+#include <mutex>
 
 template <typename T>
 class singleton
 {
 private:
-	static T  * p;
+	inline static T  * p;
+	inline static mutex m;
 public:
 	singleton() {}
 	~singleton() {}
 	static T * GetInstance()
 	{
+		lock_guard lock(m);
 		if (!p)
 			p = new T;
 		return p;
 	}
 	static void ReleaseInstance()
 	{
+		lock_guard lock(m);
 		if (p)
 			 delete p;
 		p = nullptr;
 	}
 };
-
-template <typename T>
-T * singleton<T>::p = nullptr;
